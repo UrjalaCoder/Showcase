f802765c6d868c4703b2e344f8f37db028b0b010
Tuomas Myllymaki <myllymakituomas1998@gmail.com>
2018-10-18 14:20:31 +0300

    Added todolist

---
+++ TodoList/server.js
@@ -0,0 +1,160 @@
+const fs = require("fs");
+const path = require('path');
+const http = require('http');
+const qs = require('querystring');
+
+function User(name, initialList) {
+    this.name = name;
+    this.list = initialList;
+}
+
+User.prototype.addList = function(text) {
+    this.list.push(text);
+}
+
+User.prototype.remove = function(index) {
+    // Removes the item at index -->
+    this.list.splice(index, 1);
+}
+
+var users = {'Tuomas': new User("Tuomas", ['a', 'b', 'b', 'b', 'a', 'c'])};
+
+function loadFile(filePath, callback) {
+    let readStream = fs.createReadStream(path.join(__dirname, filePath));
+    readStream.on("error", (err) => {
+        callback(err, null);
+    });
+
+    let data = "";
+
+    readStream.on("data", (chunk) => {
+        data += chunk.toString("utf8");
+    });
+
+    readStream.on("end", () => {
+        callback(null, data);
+    });
+}
+
+function serveIndex(req, res) {
+    // console.log("Index!");
+    let indexPath = path.join("public", "index.html");
+    loadFile(indexPath, (err, data) => {
+        if(err) throw err;
+        res.writeHead(200, {'Content-Type': "text/html"});
+        res.end(data);
+    })
+}
+
+function serveStatic(req, res) {
+    // console.log("Static: " + req.url);
+    loadFile(path.join("static", req.url), (err, data) => {
+        if(req.url.split("/")[1] === "javascript") {
+            res.writeHead(200, {'Content-Type': 'text/javascript'});
+        } else {
+            res.writeHead(200, {'Content-Type': "text/css"});
+        }
+
+        res.end(data);
+    });
+}
+
+function parseRequestData(req, res, callback) {
+    let data = "";
+    req.on("data", (chunk) => data += chunk.toString("utf8"));
+    req.on("end", () => {
+        callback(data);
+    });
+}
+
+function login(req, res) {
+
+    parseRequestData(req, res, (data) => {
+        let parsedData = qs.parse(data);
+        // console.log(parsedData);
+        res.writeHead(200, {"Content-Type": "application/json"});
+        if(!parsedData.username) {
+            res.end(JSON.stringify({error: 'No username parameter!'}));
+        } else {
+            let clientName = parsedData.username;
+
+            // Add user to users if it doesn't exist -->
+            if(!users[clientName]) {
+                users[clientName] = new User(clientName, []);
+            }
+
+            // Return user object -->
+            res.end(JSON.stringify({'user': users[clientName]}));
+        }
+    });
+
+    // let data = "";
+    // req.on("data", (chunk) => data += chunk.toString("utf8"));
+    // req.on("end", () => {
+    // });
+}
+
+function remove(req, res) {
+    let data = "";
+    req.on("data", (chunk) => data += chunk.toString("utf8"));
+    req.on("end", () => {
+        let parsedData = qs.parse(data);
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        // console.log(parsedData);
+
+        if(!users[parsedData['user[name]']]) {
+            res.end(JSON.stringify({error: 'No user data!'}));
+        } else {
+            let user = users[parsedData['user[name]']];
+            let index = parsedData['index'];
+            user.remove(index);
+            // console.log(users);
+            res.end(JSON.stringify({success: true, 'new_list': user.list}));
+        }
+    });
+}
+
+function add(req, res) {
+    let data = "";
+    req.on("data", (chunk) => data += chunk.toString("utf8"));
+    req.on("end", () => {
+        let parsedData = qs.parse(data);
+        res.writeHead(200, {'Content-Type': "application/json"});
+        let response = {};
+        // If can't find user with username that is requested, send error.
+        if(Object.keys(users).indexOf(parsedData['user[name]']) === -1) {
+            response['error'] = "No user with that username!";
+        } else {
+
+            // Add new item to list -->
+            let user = users[parsedData['user[name]']];
+            let item = parsedData['item'];
+
+            user.addList(item);
+            response['new_list'] = user.list
+        }
+
+        res.end(JSON.stringify(response));
+    })
+}
+
+function router(req, res) {
+    var url = req.url;
+    if(url === "/index" || url === "/") {
+        serveIndex(req, res);
+    } else if(url === "/login" && req.method === "POST") {
+        login(req, res)
+    } else if(url === "/remove" && req.method === "POST"){
+        remove(req, res);
+    } else if(url === "/add" && req.method === "POST") {
+        add(req, res);
+    } else {
+        serveStatic(req, res);
+    }
+}
+
+let server = http.createServer((req, res) => {
+    router(req, res);
+}).listen(3000, () => {
+    console.log("Server started!");
+});
---
+++ TodoList/static/javascript/script.js
@@ -0,0 +1,135 @@
+const userInputField = $("#usernameInput");
+const submitInput = $("#submitInput");
+const errorOutput = $("#errorOutput");
+const todoList = $('#todoList');
+const listInput = $("#listInput");
+const listAddButton = $("#listAddButton");
+const addForm = $("#addForm");
+const userForm = $("#userForm");
+
+var loggedInUser = null;
+
+function generateEscapedDivContent(str) {
+    return $('<div></div>').text(str);
+}
+
+function generateListItem(itemText) {
+    let htmlText = '<li class="textItem"></li>';
+    let element = $(htmlText);
+    element.text(itemText);
+    element.on("click", handleRemove);
+    return element;
+}
+
+// Helper function to do ajax requests -->
+function ajaxPost(url, sendData, doneCallback) {
+    $.ajax({
+        method: "POST",
+        url: url,
+        data: sendData
+    }).done(doneCallback);
+}
+
+var removeInProgress = false;
+function handleRemove(e) {
+    if(removeInProgress) {
+        return;
+    }
+
+    removeInProgress = true;
+    console.log(e.target);
+    // TODO:
+    // Use index to remove element from server side -->
+    // e.parentNode
+    let siblingIndices = $(e.target).parent().children();
+    let index = -1;
+    for(let i = 0; i < siblingIndices.length; i++) {
+        if(siblingIndices[i] === e.target) {
+            index = i;
+            break;
+        }
+        // console.log(siblingIndices[i] === e.target);
+    }
+    console.log(index);
+
+    if(index < 0) return;
+
+    // console.log($.param({'index': index, 'user': loggedInUser}));
+    ajaxPost("/remove", $.param({'index': index, 'user': loggedInUser}), (response) => {
+        if(response.error) {
+            console.log(response.error);
+            return;
+        }
+
+        removeInProgress = false;
+        console.log(response['new_list']);
+        loggedInUser.list = response['new_list'];
+
+        renderList(response['new_list']);
+
+    });
+
+
+}
+
+function handleAdd(e) {
+    e.preventDefault();
+    const rawUserData = listInput.val();
+    if(rawUserData === "") {
+        errorOutput.text("Invalid list add!");
+        errorOutput.show();
+    } else {
+        errorOutput.hide();
+        // console.log("DEBUG!");
+        ajaxPost("/add", {'user': loggedInUser, 'item': rawUserData}, (response) => {
+            // console.log(response);
+            if(response.error) {
+                console.log("Error: " + response.error);
+                return;
+            }
+            loggedInUser.list = response.new_list;
+            renderList(response.new_list);
+        });
+    }
+}
+
+function renderList(list) {
+    todoList.html("");
+    for(el in list) {
+        let item = generateListItem(list[el]);
+        item.on("click", handleRemove);
+        todoList.append(item);
+    }
+}
+
+function handleLogin(e) {
+    // Prevent default form submission -->
+    e.preventDefault();
+
+    const rawUserData = userInputField.val();
+    if(rawUserData.length === 0) {
+        errorOutput.text("Invalid username!")
+        errorOutput.show();
+    } else {
+        errorOutput.hide();
+        ajaxPost("/login", {'username': rawUserData}, (response) => {
+            // If error log it and return -->
+            if(response.error) {
+                console.log(response.error)
+                return;
+            }
+
+            if(!loggedInUser) {
+                // If no error and no logged user, render the list of items -->
+                renderList(response.user.list);
+                loggedInUser = response.user;
+                addForm.show();
+                userForm.hide();
+            }
+        });
+    }
+}
+
+addForm.hide();
+submitInput.on("click", handleLogin);
+listAddButton.on("click", handleAdd);
